5789b786a4b61223c703c54fddc113b652b305ba
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-10 03:39:23 -0300

    pipeline

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,6 +2,7 @@ package aplicacao;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Programa {
@@ -12,16 +13,14 @@ public class Programa {
 		Stream<Integer> st1= list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
 		
-		Stream<String> st2 = Stream.of("Maria" , "Alex", "Bob");
-		System.out.println(Arrays.toString(st2.toArray()));
+		int sum = list.stream().reduce(0, (x,y) -> x + y);
+		System.out.println("Soma= " + sum);
 		
-		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
-		System.out.println(Arrays.toString(st3.limit(10).toArray()));
-		
-		Stream<Long> st4 = Stream.iterate(new Long[] 
-				{0L, 1L}, p -> new Long [] { p[1],p[0]+ p[1]}).map(p -> p[0]);
-		System.out.println(Arrays.toString(st4.limit(10).toArray()));
-		
+		List<Integer> newList = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
 		
 		
 	}
